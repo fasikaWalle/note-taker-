a41e7e1ccf074dfcc1e38e2d48d99128ee9a7fa1
fasikaWalle <fasikabini12@gmail.com>
2021-02-18 15:29:47 -0600

    validate user input data

--- routes/apiRoute/noteRoutes.js
+++ routes/apiRoute/noteRoutes.js
@@ -1,24 +1,26 @@
 const router=require('express').Router()
 const {notes}=require('../../db/db.json')
-const {createNotes,deleteNotes}=require('../../lib/notes')
+const {createNotes,deleteNotes,validateData}=require('../../lib/notes')
+
 router.get('/notes',(req,res)=>{
-    res.send(notes)
+    res.json(notes)
 })
-
 router.post('/notes',(req,res)=>{
-    const result=createNotes(req.body,notes)
-    res.send(result)
+    if(!validateData(req.body)){
+        res.status(404).send('The data is not formated properly')
+    }else{
+        const result=createNotes(req.body,notes)  
+        res.send(result)
+    }
 })
-
 router.delete('/notes/:id',(req,res)=>{
-const found=notes.filter(note=>note.id===req.params.id)
-console.log(found)
-if(found){
-    const result=deleteNotes(req.params.id,notes)
-    res.send(result)
-}else{
-    res.status(404).send(`no result with the id ${req.params.id}`) 
-}
-
+    const found=notes.filter(note=>note.id===req.params.id)
+    console.log()
+    if(found =='undefined' || found.length===0){
+        res.status(404).send(`no result with the id ${req.params.id}`) 
+    }else{
+        const result=deleteNotes(req.params.id,notes)
+        res.send(result)
+    }
 })
 module.exports=router
