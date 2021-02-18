6b6ec684ef470ab21f1932deab0815992b598e80
fasikaWalle <fasikabini12@gmail.com>
2021-02-18 15:01:45 -0600

    create and delete functions created in order to delete and create note

--- routes/apiRoute/noteRoutes.js
+++ routes/apiRoute/noteRoutes.js
@@ -1,16 +1,24 @@
 const router=require('express').Router()
-const db=require('../../db/db.json')
-const createNotes=require('../../lib/notes')
+const {notes}=require('../../db/db.json')
+const {createNotes,deleteNotes}=require('../../lib/notes')
 router.get('/notes',(req,res)=>{
-    res.send(db)
+    res.send(notes)
 })
 
-
 router.post('/notes',(req,res)=>{
-    // req.body.id=db.length.toString()
-    // db.pushcreateNotesinstall uuid npm package)
-    const result=createNotes(req.body,db)
+    const result=createNotes(req.body,notes)
     res.send(result)
 })
 
+router.delete('/notes/:id',(req,res)=>{
+const found=notes.filter(note=>note.id===req.params.id)
+console.log(found)
+if(found){
+    const result=deleteNotes(req.params.id,notes)
+    res.send(result)
+}else{
+    res.status(404).send(`no result with the id ${req.params.id}`) 
+}
+
+})
 module.exports=router
